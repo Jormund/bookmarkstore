92f0c832402611d67a8f2e7b421e2a9540a0330c
Jormund <etiennebotek@gmail.com>
2018-02-05 19:18:03 +0100

    cdn.rawgit url

---
+++ bookmarkstore.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @id             iitc-plugin-bookmarkstore
+// @name           IITC plugin: bookmarkstore
+// @category       Info
+// @version        0.0.1.20180205.1913
+// @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
+// @updateURL      https://cdn.rawgit.com/Jormund/bookmarkstore/master/bookmarkstore.meta.js
+// @downloadURL    https://cdn.rawgit.com/Jormund/bookmarkstore/master/bookmarkstore.user.js
+// @description    [2018-02-05-1913] Bookmarkstore
+// @include        https://ingress.com/intel*
+// @include        http://ingress.com/intel*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @grant          none
+// ==/UserScript==
