d26712000f36d4f25df51a97c5b21568bd1a37b6
Jormund <etiennebotek@gmail.com>
2018-10-26 00:38:29 +0200

    changelog 0.1.0

--- bookmarkstore.user.js
+++ bookmarkstore.user.js
@@ -2,11 +2,11 @@
 // @id             iitc-plugin-bookmarkstore
 // @name           IITC plugin: bookmarkstore
 // @category       Info
-// @version        0.1.0.20181025.2210
+// @version        0.1.0.20181026.0035
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://cdn.rawgit.com/Jormund/bookmarkstore/master/bookmarkstore.meta.js
 // @downloadURL    https://cdn.rawgit.com/Jormund/bookmarkstore/master/bookmarkstore.user.js
-// @description    [2018-10-25-2210] Bookmarkstore
+// @description    [2018-10-26-0035] Bookmarkstore
 // @include        https://ingress.com/intel*
 // @include        http://ingress.com/intel*
 // @include        https://*.ingress.com/intel*
@@ -15,6 +15,9 @@
 // @match          http://*.ingress.com/intel*
 // @grant          none
 // ==/UserScript==
+/*
+	0.1.0	Added Opt menu with Import/Export buttons
+*/
 
 function wrapper(plugin_info) {
     if (typeof window.plugin !== 'function') window.plugin = function () { };
