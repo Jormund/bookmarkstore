7d7081dc86a10086166f3835f5d8e1eff3e08343
Jormund <etiennebotek@gmail.com>
2018-10-31 19:48:26 +0100

    Activate on intel.ingress.com, changed download url to github

--- bookmarkstore.user.js
+++ bookmarkstore.user.js
@@ -2,20 +2,23 @@
 // @id             iitc-plugin-bookmarkstore
 // @name           IITC plugin: bookmarkstore
 // @category       Info
-// @version        0.1.0.20181026.0035
+// @version        0.1.1.20181031.1930
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
-// @updateURL      https://cdn.rawgit.com/Jormund/bookmarkstore/master/bookmarkstore.meta.js
-// @downloadURL    https://cdn.rawgit.com/Jormund/bookmarkstore/master/bookmarkstore.user.js
-// @description    [2018-10-26-0035] Bookmarkstore
+// @updateURL      https://github.com/Jormund/bookmarkstore/raw/master/bookmarkstore.meta.js
+// @downloadURL    https://github.com/Jormund/bookmarkstore/raw/master/bookmarkstore.user.js
+// @description    [2018-10-31-1930] Bookmarkstore
 // @include        https://ingress.com/intel*
 // @include        http://ingress.com/intel*
 // @include        https://*.ingress.com/intel*
 // @include        http://*.ingress.com/intel*
+// @include        https://intel.ingress.com/*
+// @match          https://intel.ingress.com/*
 // @match          https://*.ingress.com/intel*
 // @match          http://*.ingress.com/intel*
 // @grant          none
 // ==/UserScript==
 /*
+	0.1.1	Activate on intel.ingress.com, changed download url to github
 	0.1.0	Added Opt menu with Import/Export buttons
 */
 
