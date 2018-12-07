c70f3449286a576278d62285755bd194ee7a811a
Jormund <etiennebotek@gmail.com>
2018-12-07 19:10:17 +0100

    Update bookmarkstore.meta.js

--- bookmarkstore.meta.js
+++ bookmarkstore.meta.js
@@ -2,11 +2,11 @@
 // @id             iitc-plugin-bookmarkstore
 // @name           IITC plugin: bookmarkstore
 // @category       Info
-// @version        0.1.1.20181031.1930
+// @version        0.1.1.20181207.1909
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
-// @updateURL      https://github.com/Jormund/bookmarkstore/raw/master/bookmarkstore.meta.js
-// @downloadURL    https://github.com/Jormund/bookmarkstore/raw/master/bookmarkstore.user.js
-// @description    [2018-10-31-1930] Bookmarkstore
+// @updateURL      https://raw.githubusercontent.com/Jormund/bookmarkstore/master/bookmarkstore.meta.js
+// @downloadURL    https://raw.githubusercontent.com/Jormund/bookmarkstore/master/bookmarkstore.user.js
+// @description    [2018-12-07-1909] Bookmarkstore
 // @include        https://ingress.com/intel*
 // @include        http://ingress.com/intel*
 // @include        https://*.ingress.com/intel*
